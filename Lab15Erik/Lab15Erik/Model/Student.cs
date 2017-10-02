b40cab0906ea3127764e24252f4ddd740e44bd7b
drkrieger <eplyushko@gmail.com>
2017-10-02 10:21:43 -0700

    student model added

---
+++ Lab15Erik/Lab15Erik/Model/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab15Erik.Model
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
+        [Required]
+        public string Name { get; set; }
+
+        [Range(17, 100)]
+        public int Age { get; set; }
+
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        public string Course { get; set; }
+    }
+}
